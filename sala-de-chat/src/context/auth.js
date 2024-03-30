b2689a21527c42b7dfdb82fa6216d4898bcf9bbe
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-03-30 18:00:54 -0300

    corrigir erro

--- sala-de-chat/src/context/auth.js
+++ sala-de-chat/src/context/auth.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { loginWithGoogle } from '../services/firebase';
 import { AuthContext } from '../context/auth';
+
+const AuthContext = React.createContext();
+
+const AuthProvider = (props) => {
+    const [user, setUser] = React.useState(null);
+
+    const login = async () => {
+        const user = await loginWithGoogle();
+
+        if (!user) {
+            
+        }
+        setUser(user)
+    }
+    const value = { user, login };
+    return <AuthContext.Provider value={value}{...props} />;
+};
 
 function useAuth(){
     const value = React.useContext(AuthContext);
@@ -12,4 +30,5 @@ function useAuth(){
 
 }
 
+export { AuthContext, AuthProvider };
 export { useAuth };

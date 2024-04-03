9814504b5b3826974ee7f136d51c5a14bee6bb25
HUDEVBR <72237544+HUDEVBR@users.noreply.github.com>
2024-04-02 21:28:35 -0300

    Salas de chat criadas

---
+++ sala-de-chat/src/components/Landing/index.js
@@ -0,0 +1,3 @@
+function Landing() {
+    return <div></div>
+}
---
+++ sala-de-chat/src/data/chatRooms.js
@@ -0,0 +1,14 @@
+const chatRooms = [
+    { id: 'caes', title: '🐶 Cachorros 🐶' },
+    { id: 'comida', title: '🍔 Comida 🍔' },
+    { id: 'assuntos_gerais', title: '💬 Assuntos Gerais 💬' },
+    { id: 'noticias', title: '🗞 Notícias 🗞' },
+    { id: 'musica', title: '🎹 Música 🎹'},
+    { id: 'esportes', title: '🏈 Esportes 🏈' },
+    { id: 'programação', title: '💻 Programação 💻' },
+    { id: 'contabilidade', title: '📊 Contabilidade 📊' },
+    { id: 'religião', title: '✝️ Religião ✝️' },
+    { id: 'política', title: '👨🏾‍💼 Política 👨🏾‍💼'},
+]
+
+export { chatRooms };
